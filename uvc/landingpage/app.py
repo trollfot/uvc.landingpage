a60e29037efde32ef8490ac725bf88574dda52a7
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2011-12-19 14:05:15 +0000

    InitialImport

---
+++ uvc/landingpage/app.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# # Copyright (c) 2007-2011 NovaReto GmbH
+
+import grok
+import uvcsite
+
+from fanstatic import Library, Resource
+from zope.interface import Interface
+
+
+grok.templatedir('templates')
+
+library = Library('uvc.landingpage', 'static')
+landing_css = Resource(library, 'landing.css')
+
+
+class StartSeite(uvcsite.Page):
+    grok.context(uvcsite.IUVCSite)
+    grok.name('index')
+
+    def update(self):
+        landing_css.need()
+
+
+class IStartSeiteItems(Interface):
+    """
+    MarkerInterface StartSeite
+    """
+
+
+class StartSeiteItems(uvcsite.Menu):
+    grok.name('uvc.ssi')
+    grok.implements(IStartSeiteItems)
+
+    def my_iterator(self, l):
+        i = 0
+        s = len(l)
+        while i < s:
+            if i <= s - 2:
+                yield l[i], l[i + 1]
+            if i == s - 1:
+                yield l[i],
+            i += 2
+
+    def getTupleItems(self):
+        return self.my_iterator(self.getMenuItems())

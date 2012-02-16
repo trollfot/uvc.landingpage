7967b868e3235c1c08fa80bfaece3ca86086fa79
goschtl <goschtl@dc9e7df4-2f44-11de-bd8d-4b29057232bf>
2012-02-16 13:59:17 +0000

    changes cleanup

--- uvc/landingpage/app.py
+++ uvc/landingpage/app.py
@@ -4,6 +4,9 @@
 import grok
 import uvcsite
 
+from zope.component import getMultiAdapter
+from megrok.pagetemplate import PageTemplate
+from zope.pagetemplate.interfaces import IPageTemplate
 from fanstatic import Library, Resource
 from zope.interface import Interface
 
@@ -14,14 +17,47 @@ library = Library('uvc.landingpage', 'static')
 landing_css = Resource(library, 'landing.css')
 
 
-class StartSeite(uvcsite.Page):
+class StartSeiteMenus(uvcsite.Page):
     grok.context(uvcsite.IUVCSite)
+    grok.baseclass()
     grok.name('index')
     title = ""
     description = ""
 
     def update(self):
         landing_css.need()
+
+    def render(self):
+        template = getMultiAdapter((self, self.request), IPageTemplate)
+        return template()
+
+class StartSeite(PageTemplate):
+    grok.view(StartSeiteMenus)
+
+
+class StartSeiteAdapters(uvcsite.Page):
+    grok.context(uvcsite.IUVCSite)
+    grok.name('index')
+    title = ""
+    description = ""
+
+    def my_iterator(self, l):
+        i = 0
+        s = len(l)
+        while i < s:
+            if i <= s - 2:
+                yield l[i], l[i + 1]
+            if i == s - 1:
+                yield l[i],
+            i += 2
+
+    def render(self):
+        template = getMultiAdapter((self, self.request), IPageTemplate)
+        return template()
+
+
+class StartSeiteTemplate(PageTemplate):
+    grok.view(StartSeiteAdapters)
 
 
 class IStartSeiteItems(Interface):
